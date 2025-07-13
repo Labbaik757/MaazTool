09114a5184d90504ac2ed79bacd184261ccc0b45
Kashif Ali <technicalkashifmehrvi@gmail.com>
2025-07-13 22:00:33 +0500

    Update maaz.sh

--- maaz.sh
+++ maaz.sh
@@ -2,13 +2,15 @@
 
 clear echo -e "\e[92m" figlet -f slant "MAAZ TOOL" echo -e "\e[93mOne-Command Installer by MaazTool Devs" echo -e "\e[90m======================================"
 
+echo -e "\e[94m[+] Redirecting to WhatsApp support group..." xdg-open "https://wa.me/923079741690" > /dev/null 2>&1 sleep 2
+
 echo -e "\e[92m[+] Updating Termux packages..." pkg update -y && pkg upgrade -y
 
 echo -e "\e[92m[+] Installing Python and dependencies..." pkg install python -y pip install requests pip install futures
 
-echo -e "\e[92m[+] Cloning tool from GitHub..." git clone https://github.com/Labbaik757/Labbaik-Tool.git
+echo -e "\e[92m[+] Cloning tool from GitHub..." git clone https://github.com/Labbaik757/MaazTool.git
 
-cd Labbaik-Tool
+cd maaz.sh
 
 chmod +x * echo -e "\e[93m[✓] Installation complete. Starting tool..."
 
